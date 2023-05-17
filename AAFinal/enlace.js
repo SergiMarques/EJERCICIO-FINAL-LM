102b87dcdcf6c9fdf9138465dad410bc0bac28a2
Sergi Marques <smarques2@nigul.cide.es>
2023-05-17 20:15:09 +0200

    Detalles Productos echos

--- AAFinal/enlace.js
+++ AAFinal/enlace.js
@@ -1,35 +1,25 @@
-// Obtener los botones de "Agregar al carrito"
 var addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-// Agregar un listener de click a cada botón
 addToCartButtons.forEach(function(button) {
   button.addEventListener('click', addToCart);
 });
 
-// Función para agregar un producto al carrito
 function addToCart(event) {
   console.log('Agregar al carrito');
   var product = event.target.parentNode;
   var productName = product.querySelector('h3').textContent;
   var productPrice = product.querySelector('p').textContent;
-  var productImage = product.querySelector('img').src; // Obtener la URL de la imagen
+  var productImage = product.querySelector('img').src;
 
-  // Crear un objeto con la información del producto
   var cartItem = {
     name: productName,
     price: productPrice,
-    image: productImage // Agregar la URL de la imagen al objeto
+    image: productImage
   };
 
-  // Obtener el carrito de la sesión actual o crear uno nuevo si no existe
   var cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-
-  // Agregar el producto al carrito
   cart.push(cartItem);
-
-  // Guardar el carrito actualizado en la sesión
   sessionStorage.setItem('cart', JSON.stringify(cart));
 
-  // Mostrar un mensaje de éxito
   alert('El producto ha sido agregado al carrito.');
 }
